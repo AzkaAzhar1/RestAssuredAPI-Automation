2c5d89cde4918d77c5f7eb071f3be6e75c7f7744
Azka Azhar <azka.azhar@yahoo.com>
2025-09-25 18:11:39 +0500

    CreateUser

--- src/test/java/Requests/HTTPRequests.java
+++ src/test/java/Requests/HTTPRequests.java
@@ -4,7 +4,10 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*; 
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map; 
 
 /* given() content type, set cookies, add auth, add pram, set headers info etc
 
@@ -13,10 +16,11 @@ when() get, put, post, patch, delete
 then() validate status code, extract response, extract header cookies, & response body
 */
 
-
 public class HTTPRequests {
-
-	@Test 
+	
+	String id;
+	
+	@Test (priority = 1)
 	void getusers() {
 		
 		given()
@@ -29,4 +33,54 @@ public class HTTPRequests {
 		         .body("page", equalTo(2))
 		         .log().all();
 	}
+	
+	@Test(priority = 2)
+	void CreateUser() {
+	    Map<String, String> hm = new HashMap<>();
+	    hm.put("name", "azka");
+	    hm.put("job", "QA Engineer");
+
+	 id = given()
+	             .contentType("application/json")
+	             .body(hm)
+		
+		  .when()
+		         .post("https://reqres.in/api/users")
+		         .jsonPath().getString("id");
+		
+		
+	}
+	
+   @Test(priority = 3, dependsOnMethods= {"CreateUser"})
+	void UpdateUser() {
+	    Map<String, String> hm = new HashMap<>();
+	    hm.put("name", "azkaa");
+	    hm.put("job", "QA Analyst");
+	    
+	    given()
+	           .contentType("application/json")
+	           .body(hm)
+	           
+	   .when()
+              .put("https://reqres.in/api/users/" + id)   // use dynamic id
+	  
+	   .then()
+	        .statusCode(200)
+	       .log().all();
+	} 
+   
+   @Test (priority = 4)
+   void DeleteUser() {
+	   
+	   
+	   given()
+	  
+	   .when()
+	        .delete("https://reqres.in/api/users/"+ id)
+	 
+	   .then()
+	         .statusCode(204)
+	         .log().all();
+	   
+   } 
 }
