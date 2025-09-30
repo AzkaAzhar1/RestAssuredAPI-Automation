a2c6799b26e84eb160fdf385c7d24930b367f96b
Azka Azhar <azka.azhar@yahoo.com>
2025-09-30 16:03:57 +0500

    Post Request with JSON Orj

---
+++ src/test/java/Requests/OrgJson.java
@@ -0,0 +1,41 @@
+package Requests;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.json.JSONObject;
+
+public class OrgJson {
+
+	
+	/* 
+	Post Request using ORG Json
+	*/
+	
+	@Test (priority = 1)
+	void testPostUsingOrgJsonLibrary() {
+		
+		JSONObject data = new JSONObject();
+		data.put("name", "Azka");
+		data.put("job", "QA Engineer");
+		
+		given()
+              .contentType("application/json")
+              .body(data.toString())
+
+        .when()
+               .post("https://reqres.in/api/users")
+
+        .then()
+               .statusCode(201)
+		       .body("name", equalTo("Azka"))
+		       .body("job", equalTo("QA Engineer"))
+		       .log().all();
+		
+}
+		
+		
+		
+	}
+	
