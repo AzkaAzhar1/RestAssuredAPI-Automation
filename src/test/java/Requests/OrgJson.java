5295b472c47f5fe736fe5ed4ddaf79d212ea8e05
Azka Azhar <azka.azhar@yahoo.com>
2025-10-02 00:09:56 +0500

    Post request using external json file

--- src/test/java/Requests/OrgJson.java
+++ src/test/java/Requests/OrgJson.java
@@ -1,10 +1,15 @@
 package Requests;
 import org.testng.annotations.Test;
 
+import java.io.FileReader;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class OrgJson {
 
@@ -14,7 +19,7 @@ public class OrgJson {
 	*/
 	
 	//@Test (priority = 1)
-	void testPostUsingOrgJsonLibrary() {
+	void testPostUsingOrgJsonLibrary() { //post request using ORG Json
 		
 		JSONObject data = new JSONObject();
 		data.put("name", "Azka");
@@ -35,8 +40,8 @@ public class OrgJson {
 		
 }
 	
-	@Test
-	void testPostUsingPOJO() {
+	//@Test
+	void testPostUsingPOJO() { //post request using POJO class 
 		
 		POJOclass data = new POJOclass();
 		data.setName("azka");
@@ -55,6 +60,33 @@ public class OrgJson {
 	       .body("job", equalTo("QA Engineer"))
 	       .log().all();
 	
+		
+	}
+	
+	@Test
+	void testPostUsingExternalJsonFile()  throws FileNotFoundException {
+		
+		File f = new File(".\\BODY.JSON"); //file location 
+		
+		FileReader fr = new FileReader(f); //file reader class
+		
+		JSONTokener jt = new JSONTokener(fr); //breaks the file content into json tokens (parses raw text into json structure) 
+		
+		JSONObject data = new JSONObject(jt); //convert the parsed tokens into a json object 
+		
+		
+		given()
+        .contentType("application/json")
+        .body(data.toString())
+
+  .when()
+         .post("https://reqres.in/api/users")
+
+  .then()
+         .statusCode(201)
+	       .body("name", equalTo("Azka"))
+	       .body("job", equalTo("QA Engineer"))
+	       .log().all();
 		
 	}
 		
