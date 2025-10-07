c64a81021c2715fb63602884d51a392e14b940a6
Azka Azhar <azka.azhar@yahoo.com>
2025-10-07 22:57:43 +0500

    Get Cookie Info

--- src/test/java/Requests/Cookies.java
+++ src/test/java/Requests/Cookies.java
@@ -4,11 +4,15 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.Map;
+
 import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
 
 public class Cookies {
 
-	@Test
+	@Test (priority = 1)
 	void TestCookies() {
 		
 		given()
@@ -19,4 +23,32 @@ public class Cookies {
 		      .log().all();
 		
 	}
+	
+	@Test (priority = 2)
+	void getCookieInfo() {
+		
+		Response res = given()
+		
+		.when().get("https://www.google.com/");
+		
+		//get single cookie info
+		String cookie_value = res.getCookie("AEC");
+		System.out.println("Value of cookie is"+ cookie_value);
+		
+		
+	}
+	
+	void getAllCookiesInfo() {
+		
+		Response res = given()
+				
+				.when().get("https://www.google.com/");
+		Map<String, String> cookies_values = res.getCookies();
+		
+		//read all the keys and all values of cookies
+		
+		System.out.println(cookies_values.keySet());
+		
+	}
+	
 }
