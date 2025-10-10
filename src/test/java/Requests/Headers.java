d5f63d65b8921c2fab58e281d5e2bc0894abe72e
Azka Azhar <azka.azhar@yahoo.com>
2025-10-11 02:25:25 +0500

    Headers

--- src/test/java/Requests/Headers.java
+++ src/test/java/Requests/Headers.java
@@ -3,8 +3,11 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import org.testng.annotations.Test;
+
 public class Headers {
 	
+	@Test
 	void TestHeader() {
 		
 		given()
@@ -12,7 +15,13 @@ public class Headers {
 		.when()
 	           .get("https://www.google.com/")
 	      
-	      .then();
+	      .then()
+	            .header("Content-Type", "text/html; charset=ISO-8859-1")
+	            .and()
+	            .header("Content-Encoding", "gzip")
+	            .and()
+	            .header("Server", "gws");
+	            
 	}
 	
 
