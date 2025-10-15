65ba8699467ecc0310988b290584caa114fbc3a7
Azka Azhar <azka.azhar@yahoo.com>
2025-10-16 00:58:24 +0500

    VALIDATING JSON RESPONSE USING MATCHERS

--- src/test/java/Requests/ParsingJsonResponse.java
+++ src/test/java/Requests/ParsingJsonResponse.java
@@ -14,10 +14,17 @@ public class ParsingJsonResponse {
 		//Aproach1
 		
 		given()
-		
+		       .contentType("ContentType.JSON")
 		
 		.when()
-		.then();
+		       .get("https://reqres.in/api/users/2")
+		
+		.then()
+		.statusCode(200)
+		.header("Content-Type", "application/json; charset=utf-8")
+		.body("data.last_name", equalTo("Weaver"));
+		
+				
 		
 		
 	}
