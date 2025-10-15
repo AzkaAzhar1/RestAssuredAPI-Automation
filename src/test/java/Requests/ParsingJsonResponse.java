d4af225d746891b752ab5fdf3dd503b8723e102b
Azka Azhar <azka.azhar@yahoo.com>
2025-10-15 21:55:31 +0500

    Parsing Json Response

--- src/test/java/Requests/ParsingJsonResponse.java
+++ src/test/java/Requests/ParsingJsonResponse.java
@@ -1,5 +1,24 @@
 package Requests;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
 
 public class ParsingJsonResponse {
 // parsing json response body
+	
+	@Test 
+	void TestJsonResponse() {
+		
+		//Aproach1
+		
+		given()
+		
+		
+		.when()
+		.then();
+		
+		
+	}
 }
