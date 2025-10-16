4c2511581cc009fa366d580e7e925137712a244b
Azka Azhar <azka.azhar@yahoo.com>
2025-10-16 22:11:22 +0500

    VALIDATING JSON RESPONSE USING ASSERTIONS (Without using `Then

--- src/test/java/Requests/ParsingJsonResponse.java
+++ src/test/java/Requests/ParsingJsonResponse.java
@@ -3,15 +3,18 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
 
 public class ParsingJsonResponse {
 // parsing json response body
 	
-	@Test 
+	//@Test 
 	void TestJsonResponse() {
 		
-		//Aproach1
+		//Aproach1 VALIDATING JSON RESPONSE USING MATCHERS
 		
 		given()
 		       .contentType("ContentType.JSON")
@@ -28,4 +31,26 @@ public class ParsingJsonResponse {
 		
 		
 	}
+	
+	//Aproach2 VALIDATING JSON RESPONSE USING ASSERTIONS (Without using `Then()`)
+	@Test 
+	void TestingJsonResponse() {
+		Response res = given()
+	       .contentType("ContentType.JSON")
+	
+	.when()
+	       .get("https://reqres.in/api/users/2");
+	       
+	       Assert.assertEquals(res.getStatusCode(), 200);
+	       Assert.assertEquals(res.getHeader("Content-Type"), "application/json; charset=utf-8");
+	   String data =    res.jsonPath().get("data.last_name").toString();  //used toString otherwise key value will be returned in object form. 
+	     //now after getting the string value, we storing it into the data variable
+	     
+	   //now we using Assert equals to validate the response 
+	   Assert.assertEquals(data, "Weaver");
+	       
+	}
+	
+	
+	
 }
